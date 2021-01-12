3df4ce2283e9027d96667123e4ce6144a9ca666f
Xander Riga <xanderriga@live.com>
2021-01-11 21:48:52 -0800

    Move currentseries to separate module

--- iracing.py
+++ iracing.py
@@ -28,6 +28,7 @@ from .commands.save_id import SaveId
 from .commands.leaderboard import Leaderboard
 from .commands.iratings import Iratings
 from .commands.all_series import AllSeries
+from .commands.current_series import CurrentSeries
 
 
 dotenv.load_dotenv()
@@ -59,6 +60,7 @@ class Iracing(commands.Cog):
         self.leaderboard = Leaderboard(log)
         self.iratings = Iratings(log)
         self.all_series_command = AllSeries(log)
+        self.current_series = CurrentSeries(log)
         self.update_all_servers.start()
 
     @tasks.loop(hours=1, reconnect=False)
@@ -144,28 +146,7 @@ class Iracing(commands.Cog):
 
     @commands.command()
     async def currentseries(self, ctx):
-        favorites = get_guild_favorites(ctx.guild.id)
-        if not favorites:
-            await ctx.send('Follow the directions by calling `!setfavseries` to set favorite'
-                           'series before ')
-            return
-
-        series = series_from_ids(favorites, self.all_series)
-        if not series:
-            await ctx.send('Series not found, wait a minute and try again or contact an admin.')
-
-        race_week = series[0].race_week - 1  # This is 1 indexed for some reason, but the tracks aren't
-        this_week_string = build_race_week_string(race_week, series, 'This Week', log)
-        next_week_string = build_race_week_string(race_week+1, series, 'Next Week', log)
-
-        this_week_filename = f'{ctx.guild.id}_this_week.jpg'
-        next_week_filename = f'{ctx.guild.id}_next_week.jpg'
-        imgkit.from_string(this_week_string, this_week_filename)
-        imgkit.from_string(next_week_string, next_week_filename)
-        await ctx.send(file=discord.File(this_week_filename))
-        await ctx.send(file=discord.File(next_week_filename))
-        cleanup_file(this_week_filename)
-        cleanup_file(next_week_filename)
+        await self.current_series.call(ctx, self.all_series)
 
     @commands.command()
     async def addfavseries(self, ctx, series_id):
