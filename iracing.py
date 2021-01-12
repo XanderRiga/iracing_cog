3f803cdda9f917c83bb20146abbf675eb3f108fa
Xander Riga <xanderriga@live.com>
2021-01-11 21:45:06 -0800

    Move add and remove fav series to the bottom
    These don't, strictly speaking, _work_ yet, so I am pushing them off til
    later.

--- iracing.py
+++ iracing.py
@@ -143,6 +143,31 @@ class Iracing(commands.Cog):
                            'series IDs from the `!allseries` command')
 
     @commands.command()
+    async def currentseries(self, ctx):
+        favorites = get_guild_favorites(ctx.guild.id)
+        if not favorites:
+            await ctx.send('Follow the directions by calling `!setfavseries` to set favorite'
+                           'series before ')
+            return
+
+        series = series_from_ids(favorites, self.all_series)
+        if not series:
+            await ctx.send('Series not found, wait a minute and try again or contact an admin.')
+
+        race_week = series[0].race_week - 1  # This is 1 indexed for some reason, but the tracks aren't
+        this_week_string = build_race_week_string(race_week, series, 'This Week', log)
+        next_week_string = build_race_week_string(race_week+1, series, 'Next Week', log)
+
+        this_week_filename = f'{ctx.guild.id}_this_week.jpg'
+        next_week_filename = f'{ctx.guild.id}_next_week.jpg'
+        imgkit.from_string(this_week_string, this_week_filename)
+        imgkit.from_string(next_week_string, next_week_filename)
+        await ctx.send(file=discord.File(this_week_filename))
+        await ctx.send(file=discord.File(next_week_filename))
+        cleanup_file(this_week_filename)
+        cleanup_file(next_week_filename)
+
+    @commands.command()
     async def addfavseries(self, ctx, series_id):
         try:
             series_id_int = int(series_id)
@@ -172,28 +197,3 @@ class Iracing(commands.Cog):
         except:
             await ctx.send('Series ID must be a current favorite series. '
                            'Your current favorites can be found with `!currentseries`')
-
-    @commands.command()
-    async def currentseries(self, ctx):
-        favorites = get_guild_favorites(ctx.guild.id)
-        if not favorites:
-            await ctx.send('Follow the directions by calling `!setfavseries` to set favorite'
-                           'series before ')
-            return
-
-        series = series_from_ids(favorites, self.all_series)
-        if not series:
-            await ctx.send('Series not found, wait a minute and try again or contact an admin.')
-
-        race_week = series[0].race_week - 1  # This is 1 indexed for some reason, but the tracks aren't
-        this_week_string = build_race_week_string(race_week, series, 'This Week', log)
-        next_week_string = build_race_week_string(race_week+1, series, 'Next Week', log)
-
-        this_week_filename = f'{ctx.guild.id}_this_week.jpg'
-        next_week_filename = f'{ctx.guild.id}_next_week.jpg'
-        imgkit.from_string(this_week_string, this_week_filename)
-        imgkit.from_string(next_week_string, next_week_filename)
-        await ctx.send(file=discord.File(this_week_filename))
-        await ctx.send(file=discord.File(next_week_filename))
-        cleanup_file(this_week_filename)
-        cleanup_file(next_week_filename)
