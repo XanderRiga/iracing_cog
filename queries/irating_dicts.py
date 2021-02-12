28b27d916f4671b1954496c2e669481bbf6b0211
Xander Riga <xanderriga@live.com>
2021-02-11 17:17:16 -0800

    Add current irating to irating graph
    We want to have a flat line with the current irating since the last race
    on the graph otherwise the graphs start and stop at different times
    which looks odd.

--- queries/irating_dicts.py
+++ queries/irating_dicts.py
@@ -1,4 +1,5 @@
 from ..db_helpers import init_tortoise
+from datetime import datetime
 
 
 async def get_irating_dicts(guild, category):
@@ -6,6 +7,15 @@ async def get_irating_dicts(guild, category):
     async for driver in guild.drivers:
         await init_tortoise()
         irating_list = await driver.iratings.filter(category=category)
+        current_irating = await get_current_irating_now(driver, category)
+        irating_list.append(current_irating)
         irating_dicts.append({driver.discord_id: irating_list})
 
     return irating_dicts
+
+
+async def get_current_irating_now(driver, category):
+    await init_tortoise()
+    current_irating = await driver.current_irating(category)
+    current_irating.timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return current_irating
